08990bfdd7b7df6d90946d77e336830324279b1a
Raunak_Kumari <raunak@sampana.in>
2023-12-21 18:43:14 +0530

    funnel and settings

--- src/Backend/Funnel/constant.js
+++ src/Backend/Funnel/constant.js
@@ -1,3 +1,15 @@
 var selectTemplate = `SELECT * FROM templateMessages WHERE spid=? and isDeleted !=1 and isTemplate=1`;
+var getfunnel=`select * from Funnel where sp_id=? and is_deleted != 1`
 
-module.exports={selectTemplate}
+var addFunnelDaysQuery = `INSERT INTO FunnelDays (sp_id,day,Message_id,FunnelId,created_at) values ?`
+var deleteFunnelDaysQuery = `UPDATE  FunnelDays SET isDeleted=1 ,updated_at=? where sp_id=?`
+var selectFunnelDaysQuery = `SELECT * FROM FunnelDays WHERE sp_id=? and isDeleted  !=1`
+
+
+var addMessages=`INSERT INTO FunnelMessages (sp_id,FunnelId,message_content,message_media,schedule_datetime,allTime,allDays,isEnable,start_time,end_time,created_at) values ?`
+var enableMessage=`UPDATE FunnelMessages SET isEnable=?,updated_at=? WHERE Message_id=?`
+var deleteMessage=`UPDATE FunnelMessages SET isDeleted=1 ,updated_at=? WHERE Message_id=?`
+var editMessage=`UPDATE FunnelMessages SET message_content=?,message_media=?,schedule_datetime=?,allTime=?,allDays=?,isEnable=?,start_time=?,end_time=?,updated_at=? where Message_id=?`
+
+
+module.exports={selectTemplate,getfunnel,addFunnelDaysQuery,deleteFunnelDaysQuery,selectFunnelDaysQuery,addMessages,enableMessage,deleteMessage,editMessage}
