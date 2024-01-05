b4dc5a326e37713b3ddf594c816522435f67141a
Rishabh <rishabh@sampana.in>
2024-01-05 21:08:50 +0530

    funnel ui

--- src/Frontend/dashboard/containers/funnel/funnel.component.ts
+++ src/Frontend/dashboard/containers/funnel/funnel.component.ts
@@ -10,11 +10,21 @@ export class FunnelComponent implements OnInit {
   funnelDetail:boolean=false;
   ShowChannelOption:any=false;
   modalReference:any;
+  selectedTemplate:any;
+  newCampaignDetail:any;
+  getVariables:any;
+  segmentsContactList:any
+  csvContactList:any;
   funneladd:boolean=false;
   activeStep:any=1;
   channelOption:any=[
     {value:1,label:'WhatsApp Official',checked:false},
     {value:2,label:'WhatsApp Web',checked:false}];
+  allTemplatesMain: any;
+  allTemplates: any;
+  selecetdVariable: any;
+  attributesoptionFilters: any;
+  attributesoption: any;
 
   constructor(config: NgbModalConfig,private modalService: NgbModal) { }
 
@@ -83,4 +93,108 @@ toggleCampaign(){
     closeAddActionDialog() {
     this.ShowChannelOption = false;
     }
+
+    selectTemplate(template: any) {
+      this.selectedTemplate = template;
+      console.log(this.selectedTemplate);
+      var str = template.BodyText;
+      if (str) {
+        const allVariables = this.getVariables(str, "{{", "}}");
+        let allVariablesList: any = [];
+        console.log(allVariablesList);
+      
+        allVariables.map((item: any) => {
+        allVariablesList.push({ label: item, value: '' });
+        });
+      
+        this.selectedTemplate['allVariables'] = allVariablesList;
+        console.log(this.selectedTemplate);
+      }
+      }
+
+    searchTemplate(event:any){
+      let searchKey = event.target.value
+      if(searchKey.length>2){
+      var allList = this.allTemplatesMain
+      let FilteredArray = [];
+      for(var i=0;i<allList.length;i++){
+        var content = allList[i].TemplateName.toLowerCase()
+          if(content.indexOf(searchKey.toLowerCase()) !== -1){
+            FilteredArray.push(allList[i])
+          }
+      }
+      this.allTemplates = FilteredArray
+        }else{
+        this.allTemplates = this.allTemplatesMain
+      }
+    }
+  
+    filterTemplate(temType:any){
+  
+      let allList  =this.allTemplatesMain;
+      if(temType.target.checked){
+      var type= temType.target.value;
+      for(var i=0;i<allList.length;i++){
+          if(allList[i]['Category'] == type){
+            allList[i]['is_active']=1
+          }
+      }
+       }else{
+      var type= temType.target.value;
+      for(var i=0;i<allList.length;i++){
+          if(allList[i]['Category'] == type){
+            allList[i]['is_active']=0
+          }
+      }
+       }
+      var newArray=[];
+       for(var m=0;m<allList.length;m++){
+            if(allList[m]['is_active']==1){
+        newArray.push(allList[m])
+        }
+  
+       }
+       this.allTemplates= newArray
+  
+      
+    }
+
+
+	updateButtonsValue(event:any,variable:any){
+		this.selectedTemplate[variable]=event.target.value
+		console.log(this.selectedTemplate)
+	}
+	updatedAttributeOption(attribute:any,addNewCampaign:any){
+		console.log(attribute)
+		this.activeStep=3.1
+		this.closeAllModal()
+		this.modalReference = this.modalService.open(addNewCampaign,{size: 'xl', windowClass:'white-bg'});
+	    console.log(this.selectedTemplate)
+	}
+	closeAttributeOption(status:any,addNewCampaign:any){
+		if(status != 'save'){
+		}
+		this.activeStep=3.1
+		this.closeAllModal()
+		console.log(this.selectedTemplate)
+		this.modalReference = this.modalService.open(addNewCampaign,{size: 'xl', windowClass:'white-bg'});
+	    
+	}
+
+  updateAttributeValue(event:any,variable:any){
+		variable['value']=event.target.value
+		console.log(this.selectedTemplate)
+	}
+	updateFallbackAttributeValue(event:any){
+		this.selecetdVariable['value']=event.target.value
+		console.log(this.selectedTemplate)
+	}
+
+  openAttributeOption(variable:any,AttributeOption:any){
+    this.attributesoptionFilters = this.attributesoption
+    this.selecetdVariable = variable
+    console.log(variable)
+  this.closeAllModal()
+  this.modalReference = this.modalService.open(AttributeOption,{size: 'ml', windowClass:'pink-bg'});
+}
 }
