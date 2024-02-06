3fcd2c71257a96b68bf1cc0b9596e4dcfe8daa95
Raunak_Kumari <raunak@sampana.in>
2024-02-06 19:57:13 +0530

    settings

--- src/Backend/AutoDeletion.js
+++ src/Backend/AutoDeletion.js
@@ -1,19 +1,20 @@
 const cron = require('node-cron');
 const db = require('./dbhelper');
-var axios = require('axios');
 var express = require("express");
 const awsHelper = require('./awsHelper');
 var app = express();
 const bodyParser = require('body-parser');
-const cors = require('cors');
+app.use(bodyParser.json());
+const moment = require('moment');
 
 async function autoDeletion(){
     let getDataQuery=`select * from managestorage where  isDeleted !=1`;
-    if(getDataQuery.length > 0){
-        for(let data of getDataQuery){
+    let storageData=await db.excuteQuery(getDataQuery,[]);
+    if(storageData?.length > 0){
+        for(let data of storageData){
+          
             if(data.autodeletion_media != '-1'){
             let deletemedia=await  awsHelper.deleteObjectFromBucket(data.autodeletion_media,data.SP_ID);
-            console.log(deletemedia);
             }
             
             
@@ -23,28 +24,25 @@ async function autoDeletion(){
     
             // delete messages
             if(data.autodeletion_message != '-1'){
-            let deleteMessageQuery=`UPDATE Message set is_deleted=1,updated_at=? where SPID=? AND created_at <`;
+            let deleteMessageQuery=`UPDATE Message set is_deleted=1,updated_at=? where SPID=? AND created_at < ?`;
             const daysOfmessage = subtractDaysFromNow(data.autodeletion_message);
             let deletedMessage=await db.excuteQuery(deleteMessageQuery,[new Date(),data.SP_ID,daysOfmessage]);
 
-            console.log(deletedMessage);
             }
            // delete contacts
 
            if(data.autodeletion_contacts != '-1'){
-            let deleteContactQuery=`UPDATE EndCustomer set is_deleted=1,updated_at=? where SP_ID=? AND created_at <`
+            let deleteContactQuery=`UPDATE EndCustomer set isDeleted=1,updated_at=? where SP_ID=? AND created_at < ?`
 
             const daysOfcontact = subtractDaysFromNow(data.autodeletion_contacts);
-            let deletedcontact=await db.excuteQuery(deleteContactQuery,[new Date(),data.SP_ID,daysOfcontact]);
-
-            console.log(deletedcontact);
+           let deletedcontact=await db.excuteQuery(deleteContactQuery,[new Date(),data.SP_ID,daysOfcontact]);
            }
         }
     }
 
 }
 
-cron.schedule('0 12 * * *', async () => {
+cron.schedule('0 12 * * *', async () => {      
     console.log('Running scheduled task...');
     autoDeletion();
   
