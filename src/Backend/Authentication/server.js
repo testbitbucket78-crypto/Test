c52824b263319b8f7f71a5b682da742a3a97cdae
Raunak_Kumari <raunak@stacknize.com>
2024-11-26 15:27:49 +0530

    webJS SWAGGER

--- src/Backend/Authentication/server.js
+++ src/Backend/Authentication/server.js
@@ -1,13 +1,13 @@
 
-const express=require('express')
-const router=require('./router.js')
+const express = require('express')
+const router = require('./router.js')
 const bodyParser = require('body-parser');
 const db = require("../dbhelper");
-const userController=require('./user.js');
-const indexController=require('./index.js');
+const userController = require('./user.js');
+const indexController = require('./index.js');
 const val = require('./constant');
-const cors=require('cors')
-const app=express();
+const cors = require('cors')
+const app = express();
 
 app.use(cors());
 
@@ -17,59 +17,69 @@ const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express')
 
 const swaggerOptions = {
-   swaggerDefinition: {
-       openapi: "3.0.0",
-       info: {
-           title: "Gambox API Documentation",
-           version: "1.0.0",
-           description: "API documentation for Gambox project. *Note:* For authorization, enter only the raw token without 'Bearer' prefix."
-       },
-       servers: [
-           {
-               url: 'https://authapi.stacknize.com',
-               description: 'Stagging Server',
-           },
-           {
-            url: 'http://localhost:3003',
-            description: 'Stagging Server',
-        }
-       ],
-       components: {
-        securitySchemes: {
-          bearerAuth: {
-            type: "http",
-            scheme: "bearer",
-            bearerFormat: "JWT", // Optional, you can specify the token format
-          },
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "EngageKart API Documentation",
+      version: "1.0.0",
+      description: "API documentation for EngageKart project. *Note:* For authorization, enter only the raw token without 'Bearer' prefix."
+    },
+    servers: [
+      {
+        url: 'https://authapi.stacknize.com',
+        description: 'Stagging Server',
+      },
+      {
+        url: 'http://localhost:3003',
+        description: 'Stagging Server',
+      },
+      {
+        url: 'https://waweb.stacknize.com',
+        description: 'Stagging Server',
+      },
+      {
+        url: 'http://localhost:3009',
+        description: 'Stagging Server',
+      }
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT", // Optional, you can specify the token format
         },
       },
-      security: [
-        {
-          bearerAuth: [],
-        },
-      ],
-       
-       tags: [
-           { name: 'Authentication' },
-         
-       ],
-   },
-   apis: [
-       './router.js'
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
 
-   ]
+    tags: [
+      { name: 'Authentication' },
+      {name: 'webJS'}
+
+    ],
+  },
+  apis: [
+    './router.js',
+    '../webJS/whatsAppWeb.js'
+
+  ]
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs, {
-   swaggerOptions: {
-       deepLinking: false,
-   },
+  swaggerOptions: {
+    deepLinking: false,
+  },
 }));
-app.use('/',router);
+app.use('/', router);
 
 
 app.listen(3003, () => {
-    console.log('Server is running on port 3003');
+  console.log('Server is running on port 3003');
 });
 
