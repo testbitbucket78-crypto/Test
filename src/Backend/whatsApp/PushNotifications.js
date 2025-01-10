87182328b947737351a10a0afb4c502004022fcf
ayushjidhanta <ayush@stacknize.com>
2025-01-10 18:32:26 +0530

    removed duplicate method from push notification

--- src/Backend/whatsApp/PushNotifications.js
+++ src/Backend/whatsApp/PushNotifications.js
@@ -126,13 +126,6 @@ async function NotifyServer(display_phone_number, updatemessage, message, status
 
     socket.emit('message', notificationMsg);
 
-    if(message){
-      let data = await db.excuteQuery('select SPID,assignAgent from Message where Message_id =?', msg_id);
-      if(data?.assignAgent == -3){
-      let websocketurl = await db.excuteQuery('select webhook_url from UserAPIKeys where spid =?', data?.spid);   
-        sendDataToWebHook(websocketurl,notificationMsg);
-      }
-    }
   } catch (err) {
     console.error("Notify Error:", err);
   }
