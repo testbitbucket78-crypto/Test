70aa210c1934a05f77ea69c2bdf5796a3477b96d
ayushjidhanta <ayush@stacknize.com>
2025-02-04 00:02:18 +0530

    default message issue + Account Setting Register webhook + channel No necessary update.

--- src/Backend/common/constant.js
+++ src/Backend/common/constant.js
@@ -3,5 +3,9 @@ const variables = {
           auth: 'https://authapi.stacknize.com',
           waweb: 'https://waweb.stacknize.com',
         },
+        webhookRegisterPayload: {
+          callback_url: 'https://call.stacknize.com/webhook',
+          verify_token: 'raunak'
+      }
 }
 module.exports = variables;
--- src/Backend/settings/constant.js
+++ src/Backend/settings/constant.js
@@ -440,7 +440,7 @@ var isEnableQuery=`UPDATE APIToken SET isEnable=? ,updated_at=? where id=?`
 var insertIPAddress=`INSERT INTO APIIPAddress (spid,token_id,IPAddress,created_at,is_Deleted) values ?`
 
 //____________________________________BusinessHealth____________________________//
-var insertHealthStatus = `INSERT INTO businessHealth (phone_number_id, channel_phone_number, balance_limit_today, quality_rating, created_at, fb_verification, SP_ID) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+var insertHealthStatus = `INSERT INTO businessHealth (phone_number_id, channel_phone_number, balance_limit_today, quality_rating, created_at, fb_verification, SP_ID, isWebhookRegistered) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
 var selectHealthStatus = `SELECT * FROM businessHealth WHERE phone_number_id = ? and SP_ID = ?`
 
 //---------------------------------UserAPIKeys-----------------------------------//
